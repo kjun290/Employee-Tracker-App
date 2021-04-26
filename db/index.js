58d8ae305018d96a082b9348840effa7df27cf64
Kevin Jun <kwjun90@gmail.com>
2021-04-26 18:13:42 -0500

    completed class DB

--- db/index.js
+++ db/index.js
@@ -9,10 +9,45 @@ class DB {
         return this.connection.query(
         "SELECT * from employee"
         )
-    }
+    };
 
+    addDepartment(department) {
+        return this.connection.query(
+            "INSERT INTO department SET ?", department
+        )
+    };
 
+    addRole(role) {
+        return this.connection.query(
+            "INSERT INTO role SET ?", role
+        )
+    };
+
+    addEmployee(employee) {
+        return this.connection.query(
+            "INSERT INTO employee SET ?", employee
+        )
+    };
+
+    viewDepartment() {
+        return this.connection.query(
+            "SELECT * FROM department"
+        )
+    };
+
+    viewRoles() {
+        return this.connection.query(
+            "SELECT * FROM roles"
+        )
+    };
+    
+    viewEmployee() {
+        return this.connection.query(
+            "SELECT * FROM employee"
+        )
+    };
 
 }
 
 module.exports = new DB(connection)
+
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const db = require("./db");
 require("console.table");
+const inquirer = require("inquirer");
+const { connection } = require("./db");
 
 
 console.log("My app is running")
+
 
 
 async function viewEmployees() {
@@ -10,4 +13,44 @@ async function viewEmployees() {
     console.table(allEmployees)
 }
 
-viewEmployees()
+const mainMenu = () => {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do",
+        choices: ["add a department", "add a role", "add an employee", "view departments", "view roles", "view an employee", "update employee's role", "exit"
+        ]
+    })
+
+    .then((choice) => {
+        switch (answer.action) {
+            case "add a department":
+                addDepartment();
+                break;
+
+            case "add a role":
+                addRole();
+                break;
+            case "add an employee":
+                addEmployee();
+                break;
+            case "view departments":
+                viewDepartment();
+                break;
+            case "view roles":
+                viewRoles()
+                break;
+            case "view an employee":
+                viewEmployees();
+                break;
+            case "update employee's role":
+                updateEmployee();
+                break;
+            default:
+                connection.end();
+        }
+    })
+
+}
+viewEmployees();
+mainMenu()
