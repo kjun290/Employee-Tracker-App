6986ea137d834c20550e7530dcf66a17ae209f79
Kevin Jun <kwjun90@gmail.com>
2021-04-26 23:14:03 -0500

    completed the addDepartment function

--- index.js
+++ index.js
@@ -14,20 +14,28 @@ async function viewEmployees() {
 }
 
 const mainMenu = () => {
-    inquirer.prompt({
+    inquirer
+    .prompt({
         name: "action",
         type: "list",
         message: "What would you like to do",
-        choices: ["add a department", "add a role", "add an employee", "view departments", "view roles", "view an employee", "update employee's role", "exit"
+        choices: [
+            "add a department",
+          "add a role", 
+          "add an employee", 
+          "view departments", 
+          "view roles", 
+          "view an employee", 
+          "update employee's role",
+           "exit"
         ]
     })
 
-    .then((choice) => {
+    .then((answer) => {
         switch (answer.action) {
             case "add a department":
                 addDepartment();
                 break;
-
             case "add a role":
                 addRole();
                 break;
@@ -52,5 +60,42 @@ const mainMenu = () => {
     })
 
 }
+
+const addDepartment = () => {
+    inquirer
+    .prompt ({
+        name: "add a department",
+        type: "input",
+        message: "What new department would you like to add?"
+    })
+
+    .then ((answer) => {
+        const department = {
+            departmentName: answer.addDepartment
+        }
+        db.addDepartment(department).then(res => {
+            console.table(res)
+        })
+    })
+}
+
+async function addRole() {
+    let viewDepartment = await db.viewDepartment()
+    console.table(viewDepartment)
+
+}
+
+    inquirer
+    .prompt([
+        {
+            name:"role",
+            type: "input",
+            message: "What role would you like to add?"
+
+        }
+
+        
+    ])
+
 viewEmployees();
 mainMenu()
